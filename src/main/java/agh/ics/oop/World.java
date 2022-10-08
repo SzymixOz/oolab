aca363de4a8b8b23749318364c1124944b19855d
SzymixOz <110102349+SzymixOz@users.noreply.github.com>
2022-10-08 23:57:38 +0200

    Add files via upload

---
+++ src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,36 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+        run(change(args));
+        System.out.println("system zakończył działanie");
+    }
+
+    private static Direction[] change(String[] args) {
+        Direction[] dir = new Direction[args.length];
+        int i = 0;
+        for (String arg : args) {
+            switch (arg) {
+                case "f" -> dir[i] = Direction.FORWARD;
+                case "b" -> dir[i] = Direction.BACKWARD;
+                case "r" -> dir[i] = Direction.RIGHT;
+                case "l" -> dir[i] = Direction.LEFT;
+            }
+            i++;
+        }
+        return dir;
+    }
+
+    private static void run(Direction[] temp) {
+        for (Direction d : temp) {
+            String message = switch (d) {
+                case FORWARD -> "Zwierzak idzie do przodu";
+                case BACKWARD -> "Zwierzak idzie do tyłu";
+                case RIGHT -> "Zwierzak skręca w prawo";
+                case LEFT -> "Zwierzak skręca w lewo";
+            };
+            System.out.println(message);
+        }
+    }
+}
